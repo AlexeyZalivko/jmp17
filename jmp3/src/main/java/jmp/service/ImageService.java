7e48324a2cb114228e5e146e2144ca8c4f0d6804
Alex Lybetsky <lybetsky.alex@gmail.com>
2017-03-18 20:23:31 +0300

    All download logo service

--- jmp3/src/main/java/jmp/service/ImageService.java
+++ jmp3/src/main/java/jmp/service/ImageService.java
@@ -6,15 +6,11 @@ import com.sun.jersey.multipart.FormDataParam;
 import jmp.database.dao.UserBean;
 import jmp.database.dao.UserBeanImpl;
 import jmp.database.data.User;
+import org.apache.commons.lang3.StringUtils;
 
-import javax.imageio.ImageIO;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.logging.Logger;
@@ -25,6 +21,7 @@ import java.util.logging.Logger;
 @Path("/images")
 public class ImageService {
 
+    public static final String ERROR_USER_HASN_T_LOGO = "That user hasn't logo";
     private static Logger log = Logger.getLogger(UserBeanImpl.class.getName());
 
     private UserBean userBean = new UserBeanImpl();
@@ -32,9 +29,31 @@ public class ImageService {
     private Gson gson = new Gson();
 
     public static final String USER_HOME = "/home/alex";
-    public static final String PNG_EXTENSION = ".png";
     public static final String FS_SEPARATOR = "/";
     public static final String UNDERLINE = "_";
+
+    @GET
+    @Path("/{id}")
+    public Response getImageByUserId(@PathParam("id") final Long id) {
+        File file = null;
+        String fileName = null;
+        try {
+            final User user = userBean.getUserById(id);
+
+            if (user == null || StringUtils.isEmpty(user.getLogo())) {
+                return Response.status(Response.Status.BAD_REQUEST).entity(ERROR_USER_HASN_T_LOGO).build();
+            }
+
+            file = new File(getBasePath(user.getLogo(), id.toString()));
+            fileName = user.getLogo();
+        } catch (SQLException e) {
+            log.warning(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        final Response.ResponseBuilder responseBuilder = Response.ok((Object) file);
+        responseBuilder.header("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        return responseBuilder.build();
+    }
 
     @POST
     @Path("/")
@@ -59,7 +78,7 @@ public class ImageService {
 
     private void saveImage(final InputStream imageStream, final FormDataContentDisposition form, final String id) throws IOException {
         OutputStream ostream = null;
-        final String resultPath = getBasePath(form, id);
+        final String resultPath = getBasePath(form.getFileName(), id);
         final File file = new File(resultPath);
         ostream = new FileOutputStream(file);
         byte[] b = new byte[1024];
@@ -67,10 +86,12 @@ public class ImageService {
         while ((length = imageStream.read(b)) != -1) {
             ostream.write(b, 0, length);
         }
+        ostream.flush();
+        ostream.close();
     }
 
-    private String getBasePath(final FormDataContentDisposition form, final String id) {
-        final String name = USER_HOME + FS_SEPARATOR + id + UNDERLINE + form.getFileName();
+    private String getBasePath(final String fileName, final String id) {
+        final String name = USER_HOME + FS_SEPARATOR + id + UNDERLINE + fileName;
         log.info("Image name: " + name);
         return name;
     }
